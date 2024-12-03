f98217585e86cd4fa6700c67a6fbb87e859b2481
Joshua Kaitharam Thomas <100039717+ZaDukk@users.noreply.github.com>
2024-12-03 22:42:40 +0000

    Update day03.py

--- day03.py
+++ day03.py
@@ -1,11 +1,4 @@
 import re
-def extract_integers(input_string, allow_negative=False):
-    pattern = r'-?\d+' if allow_negative else r'\d+'
-    integers = re.findall(pattern, input_string)
-    integers = [int(num) for num in integers]
-
-    return integers
-
 with open("input.txt","r") as f:
      inp = [line.strip() for line in f.readlines()]
 
